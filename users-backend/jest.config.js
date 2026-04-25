d271ea16867d02c197cc65f8ef89145d60ea88d7
Prakash453345 <prakashraj.badugula@gmail.com>
2026-04-25 15:26:02 +0530

    Split app: Retain only admin/caller and backend modules

--- users-backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  setupFilesAfterEnv: ['./tests/setup.js'],
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
-  // Silence console noise during tests
-  silent: false,
-};
