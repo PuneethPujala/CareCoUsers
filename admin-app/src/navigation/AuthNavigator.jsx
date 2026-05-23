fa67c93a669149e7ce63bf13d83982fd15cb4886
Prakash453345 <prakashraj.badugula@gmail.com>
2026-05-23 16:26:36 +0530

    Fix Android hexagon styling bug and make splash screen the true AuthNavigator landing route

--- admin-app/src/navigation/AuthNavigator.jsx
+++ admin-app/src/navigation/AuthNavigator.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LandingScreen from '../screens/LandingScreen';
+import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
 
@@ -8,8 +8,10 @@ const Stack = createNativeStackNavigator();
 
 export default function AuthNavigator() {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Landing" component={LandingScreen} />
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
+            <Stack.Screen name="Splash">
+                {(props) => <SplashScreen {...props} onFinish={() => props.navigation.navigate('Login')} />}
+            </Stack.Screen>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         </Stack.Navigator>
